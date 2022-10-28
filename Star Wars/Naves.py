71c650821995ef8803632437b21fc63b23c89f49
carlospuigserver <cpuiggut@myuax.com>
2022-10-28 17:18:00 +0200

    commit

---
+++ Star Wars/Naves.py
@@ -0,0 +1,16 @@
+import csv
+
+Naves=[
+    ['Nombre', 'Longitud', 'tripulacion'],
+    ['Nave de pasajeros C-3','409','290'],
+    ['Halcon Milenario',' 34,37','4'],
+    ['Nave de aterrizaje C-9979','210','361'],
+    ['Crucero ligero MC40a','550','600'],
+
+]
+
+
+with open ('Naves.csv','w',newline='') as file:
+    writer = csv.writer(file,delimiter=';')
+    #escribir
+    writer.writerows(Naves)
--- polinomios/polinomio.py
+++ polinomios/polinomio.py
@@ -19,6 +19,68 @@ class Polinomio(object):
 
 
 
+def agregar_termino(polinomio, termino, valor):
+    
+    aux = Nodo()
+    dato = datoPolinomio(valor,termino)
+    aux.info = dato
+    if (termino > polinomio.grado):
+        aux.sig = polinomio.termino_mayor
+        polinomio.termino_mayor = aux
+        polinomio.grado = termino
+    else:
+        actual = polinomio.termino_mayor
+        while(actual.sig is not None and termino < actual.sig.info.termino):
+            actual = actual.sig
+        aux.sig = actual.sig
+        actual.sig = aux
+
+
+
+
+def modificar(polinomio, termino, valor):
+    aux = polinomio.termino_mayor
+    while (aux is not None and aux.info.termino != termino):
+        aux = aux.sig
+    aux.info.valor = valor
+
+
+
+#RESTA
+def resta(polinomio1, polinomio2):
+   
+    paux = Polinomio()
+    mayor = polinomio1 if (polinomio1.grado > polinomio2.grado) else polinomio2
+    for i in range(0, mayor.grado +1):
+        total = determinar_valor(polinomio1, i) - object(polinomio2, i)
+        if (total != 0):
+            agregar_termino(paux, i, total)
+    return paux
+
+
+
+#DIVISIÓN
+def dividir(polinomio1, polinomio2):
+    
+    paux = Polinomio()
+    pol1 = polinomio1.termino_mayor
+    while (pol1 is not None):
+        pol2 = polinomio2.termino_mayor
+        while (pol2 is not None):
+            termino = pol1.info.termino + pol2.info.termino
+            valor = pol1.info.valor / pol2.info.valor
+            if (determinar_valor(paux, termino) != 0):
+                valor += determinar_valor(paux, termino)
+                modificar(paux, termino, valor)
+            else:
+                agregar_termino(paux, termino, valor)
+            pol2 = pol2.sig
+        pol1 = pol1.sig
+    return 
+
+
+
+
 #DETERMINAR VALOR
 def determinar_valor(polinomio, termino):
    
@@ -29,6 +91,10 @@ def determinar_valor(polinomio, termino):
         return aux.info.valor
     else:
         return 0
+
+
+
+
 
 
 
