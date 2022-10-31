4314e10e93d38918f22c946588fd02195f428305
carlospuigserver <cpuiggut@myuax.com>
2022-10-31 10:15:05 +0100

    commit

--- Star Wars/Naves.py
+++ Star Wars/Naves.py
@@ -1,17 +1,47 @@
-import csv
+class Nave:
+    def __init__(self,nombre,longitud,tripulacion,pasajeros):
+        self.nombre=nombre
+        self.longitud=longitud
+        self.tripulacion=tripulacion
+        self.pasajeros=pasajeros
 
-Naves=[
-    ['Nombre', 'Longitud', 'tripulacion'],
-    ['Nave de pasajeros C-3','409','290'],
-    ['Halcon Milenario',' 34,37','4'],
-    ['Nave de aterrizaje C-9979','210','361'],
-    ['Crucero ligero MC40a','550','600'],
-    ['Estrella de la Muerte','105','1500'],
+class EstrellaMuerte(Nave):
+    def __init__(self):
+        self.nombre="Estrella de la Muerte"
+        self.longitud=120000
+        self.tripulacion=1350000
+        self.pasajeros=1500000
 
-]
+    def __str__(self):
+        print("Toda la información de la Estrella de la muerte es: \n")
+        f'Longitud =   {self.longitud} metros \n'
+        f'Tripulación =   {self.tripulacion} personas \n'
+        f'Pasajeros =   {self.pasajeros} personas \n'
 
 
-with open ('Naves.csv','w',newline='') as file:
-    writer = csv.writer(file,delimiter=';')
-    #escribir
-    writer.writerows(Naves)
+class HalconMilenario(Nave):
+    def __init__(self):
+        self.nombre="Halcón Milenario"
+        self.longitud=34
+        self.tripulacion=4
+        self.pasajeros=10
+    
+    def __str__(self):
+        print("Toda la información del Halcón Milenario es: \n")
+        f'Longitud =   {self.longitud} metros \n'
+        f'Tripulación =   {self.tripulacion} personas \n'
+        f'Pasajeros =   {self.pasajeros} personas \n'
+
+class ATAP(Nave):
+    def __init__(self):
+        self.nombre="AT-AP"
+        self.longitud=25
+        self.tripulacion=3
+        self.pasajeros=13
+
+class CañoneraLAAT(Nave):
+    def __init__(self):
+        self.nombre="Cañonera LAAT"
+        self.longitud=17
+        self.tripulacion=4
+        self.pasajeros=30
